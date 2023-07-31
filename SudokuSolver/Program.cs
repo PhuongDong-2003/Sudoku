c0b715fb64eae3f63ec369a4520b5a1459eb7b95
PhuongDong-2003 <nguyenphuongdong4102003@gmail.com>
2023-07-31 10:11:02 +0700

    first commit

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Để giản lược, ta chỉ đưa một ví dụ Sudoku để giải
+            int[,] sudokuGrid = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+            };
+
+            if (SolveSudoku(sudokuGrid))
+            {
+                Console.WriteLine("Sudoku solved:");
+                PrintSudoku(sudokuGrid);
+            }
+            else
+            {
+                Console.WriteLine("No solution found for the Sudoku.");
+            }
+        }
+
+        static bool SolveSudoku(int[,] grid)
+        {
+            // Implement your Sudoku solving algorithm here
+            // (This is just a placeholder for demonstration purposes)
+            // You can use backtracking or other algorithms to solve the Sudoku puzzle.
+
+            // Replace this return statement with your solving logic
+            return false;
+        }
+
+        static void PrintSudoku(int[,] grid)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(grid[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
